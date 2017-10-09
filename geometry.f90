72b70b88d578cc8f6d165b0bf0bff7927bc69571
Corentin Cadiou <contact@cphyc.me>
2017-10-09 20:09:13 +0200

    initial commit

---
+++ geometry.f90
@@ -0,0 +1,289 @@
+module CylinderGeometry
+
+  implicit none
+  ! private
+
+  integer, parameter :: dp = selected_real_kind(15)
+  real(dp), parameter :: pi = atan(1._dp) * 4._dp
+  integer :: niter = 1000
+
+  type :: Cylinder_t
+     real(dp), dimension(3) :: center
+     real(dp), dimension(3) :: normal1
+     real(dp), dimension(3) :: normal2
+     real(dp), dimension(3) :: normal3
+     real(dp) :: r, h
+   contains
+     procedure :: check => cylinderCheck
+     procedure :: intersectionVolume
+     procedure :: volume => cylinderVolume
+     procedure :: draw => cylinderDraw
+     procedure :: contains => cylinderContains
+     procedure :: project => cylinderProject
+  end type Cylinder_t
+
+  type :: Cube_t
+     real(dp), dimension(3) :: center
+     real(dp), dimension(3) :: normal1
+     real(dp), dimension(3) :: normal2
+     real(dp), dimension(3) :: normal3
+     real(dp) :: a
+   contains
+     procedure :: check => cubeCheck
+     procedure :: volume => cubeVolume
+     procedure :: draw => cubeDraw
+     procedure :: contains => cubeContains
+     procedure :: project => cubeProject
+  end type Cube_t
+
+  type ptCube_t
+     real(dp) :: x, y, z
+  end type ptCube_t
+
+  type ptCylinder_t
+     real(dp) :: r, z
+  end type ptCylinder_t
+
+  public :: set_niter, Cylinder_t, Cube_t, dp
+
+contains
+
+  function cylinderVolume(self) result (volume)
+    class(Cylinder_t) :: self
+    real(dp) :: volume
+    volume = self%r**2 * pi * self%h
+  end function cylinderVolume
+
+  function cubeVolume(self) result (volume)
+    class(Cube_t) :: self
+    real(dp) :: volume
+    volume = self%a**3
+  end function cubeVolume
+
+  function cubeCheck(self) result (valid)
+    class(Cube_t) :: self
+    logical ::valid
+
+    real(dp) :: tmp
+
+    tmp = sqrt(dot_product(self%normal1, self%normal1))
+    self%normal1 = self%normal1 / tmp
+
+    tmp = sqrt(dot_product(self%normal2, self%normal2))
+    self%normal2 = self%normal2 / tmp
+
+    if (dot_product(self%normal1, self%normal2) /= 0._dp) then
+       valid = .false.
+       return
+    end if
+
+    self%normal3 = (/ &
+         self%normal1(2)*self%normal2(3) - self%normal1(3)*self%normal2(2), &
+         self%normal1(3)*self%normal2(1) - self%normal1(1)*self%normal2(3), &
+         self%normal1(1)*self%normal2(2) - self%normal1(2)*self%normal2(1) &
+         /)
+    valid = .true.
+  end function cubeCheck
+
+  function cylinderCheck(self) result(valid)
+    class(Cylinder_t) :: self
+    logical :: valid
+
+    real(dp) :: norm, n2, N(3)
+
+    n2 = dot_product(self%normal1, self%normal1)
+    norm = sqrt(n2)
+    self%normal1 = self%normal1 / norm
+    N = self%normal1
+
+    ! Build a normal perpendicular to the first one for convenience
+    if      (N(1) * N(2) /= 0.) then
+       self%normal2 = (/N(2), -N(1), 0._dp/)
+    else if (N(1) * N(3) /= 0.) then
+       self%normal2 = (/N(3), 0._dp, -N(1)/)
+    else
+       self%normal2 = (/0._dp, N(3), N(2)/)
+    end if
+    self%normal2 = self%normal2 / sqrt(dot_product(self%normal2, self%normal2))
+
+    self%normal3 = (/ &
+         self%normal1(2)*self%normal2(3) - self%normal1(3)*self%normal2(2), &
+         self%normal1(3)*self%normal2(1) - self%normal1(1)*self%normal2(3), &
+         self%normal1(1)*self%normal2(2) - self%normal1(2)*self%normal2(1) &
+         /)
+    valid = .true.
+
+  end function cylinderCheck
+
+  subroutine cylinderProject(self, pt, ptOut)
+    class(Cylinder_t) :: self
+    real(dp), dimension(3), intent(in) :: pt
+    type(ptCylinder_t), intent(out) :: ptOut
+
+    real(dp) :: tmpPt(3)
+
+    tmpPt = pt - self%center
+    ! Get height
+    ! h = X.N
+    ptOut%z = dot_product(self%normal1, tmpPt)
+
+    ! Project on plane
+    ! r = | X - X.N * N |
+    ptOut%r = sqrt(sum((tmpPt - self%normal1 * ptOut%z)**2))
+
+  end subroutine cylinderProject
+
+  subroutine cubeProject(self, pt, ptOut)
+    ! Project the point in the local frame of the cube
+    class(Cube_t) :: self
+    real(dp), dimension(3), intent(in) :: pt
+    type(ptCube_t), intent(out) :: ptOut
+
+    real(dp) :: tmpPt(3)
+
+    tmpPt = pt - self%center
+
+    ptOut%x = dot_product(self%normal1, tmpPt)
+    ptOut%y = dot_product(self%normal2, tmpPt)
+    ptOut%z = dot_product(self%normal3, tmpPt)
+  end subroutine cubeProject
+
+  function cylinderContains(self, pt) result (res)
+    class(Cylinder_t) :: self
+    real(dp), dimension(3) :: pt
+    logical :: res
+
+    type(ptCylinder_t) :: ptLoc
+
+    ! Project point in local ref
+    call self%project(pt, ptLoc)
+
+    ! Check point height
+    if (abs(ptLoc%z) > self%h) then
+       res = .false.
+       return
+    end if
+
+    ! Check point radius
+    if (ptLoc%r > self%r) then
+       res = .false.
+       return
+    end if
+
+    res = .true.
+
+  end function cylinderContains
+
+  function cubeContains(self, pt) result (res)
+    class(Cube_t) :: self
+    real(dp), dimension(3) :: pt
+    logical :: res
+
+    type(ptCube_t) :: ptLoc
+
+    real(dp) :: ao2
+
+    ! Project point in local referential
+    call self%project(pt, ptLoc)
+
+    ao2 = self%a / 2._dp
+
+    res = (abs(ptLoc%x) < ao2) .and. &
+         (abs(ptLoc%y) < ao2) .and. &
+         (abs(ptLoc%z) < ao2)
+
+  end function cubeContains
+
+  function cylinderDraw(self) result (point)
+    class(Cylinder_t) :: self
+    real(dp), dimension(3) :: point
+
+    real(dp), dimension(3) :: tmp
+    real(dp) :: r, theta
+
+
+    call random_number(tmp)
+    ! Compute r, theta
+    r = sqrt(tmp(1)) * self%r
+    theta = 2 * pi * tmp(2)
+
+    ! Project in absolute referential
+    point(:) = &
+         self%normal2 * r * cos(theta) + &
+         self%normal3 * r * sin(theta) + &
+         (tmp(3) - 0.5_dp) * 2 * self%h * self%normal1 + &
+         self%center
+
+  end function cylinderDraw
+
+  function cubeDraw(self) result (point)
+    class(Cube_t) :: self
+    real(dp), dimension(3) :: point
+
+    real(dp), dimension(3) :: tmp
+
+    call random_number(tmp)
+    tmp = (tmp - 0.5) * 2 * self%a
+    point = self%normal1 * tmp(1) + &
+         self%normal2 * tmp(2) + &
+         self%normal3 * tmp(3) + &
+         self%center
+
+  end function cubeDraw
+
+
+  subroutine intersectionVolume(cyl, cube, volume, intersect)
+    class(Cylinder_t), intent(in) :: cyl
+    type(Cube_t), intent(in) :: cube
+    real(dp), intent(out) :: volume
+    logical, intent(out)  :: intersect
+
+    real(dp) :: d2, Vcyl, Vcube
+    real(dp), dimension(3) :: pt
+    integer :: nin, ntot
+
+
+    d2 = max(cyl%r, cyl%h)**2 + 3*cube%a**2
+
+    ! If centers too far, just return
+    if (sum((cyl%center - cube%center)**2) > d2) then
+       intersect = .false.
+       volume = 0.0_dp
+    end if
+
+    ! Volumes may overlap. Draw random points from the smallest volume
+    nin = 0
+    ntot = 0
+
+    Vcyl = cyl%volume()
+    Vcube = cube%volume()
+    if (Vcyl < Vcube) then
+       do while (ntot < niter)
+          pt = cyl%draw()
+          ntot = ntot + 1
+          if (cube%contains(pt)) then
+             nin = nin + 1
+          end if
+       end do
+       volume = Vcyl * nin / ntot
+    else
+       do while (ntot < niter)
+          pt = cube%draw()
+          ntot = ntot + 1
+          if (cyl%contains(pt)) then
+             nin = nin + 1
+          end if
+       end do
+       volume = Vcube * nin / ntot
+    end if
+
+    intersect = volume > 0._dp
+
+  end subroutine intersectionVolume
+
+  subroutine set_niter(n)
+    integer, intent(in) :: n
+    niter = n
+  end subroutine set_niter
+
+end module CylinderGeometry
---
+++ test.f90
@@ -0,0 +1,138 @@
+program test
+  use CylinderGeometry
+  use, intrinsic :: iso_fortran_env, only : stdin=>input_unit, &
+       stdout=>output_unit, &
+       stderr=>error_unit
+
+  type(Cylinder_t) :: cyl
+  type(Cube_t) :: cube
+
+  logical :: intersect
+  real(dp) :: volume
+
+  cyl%center = (/1., 1., 1./)
+  cyl%r = 1.0
+  cyl%h = 1.0
+  cyl%normal1 = (/1., 1., 1./)
+
+  !----------------------------------------
+  ! Testing cylinder
+  !----------------------------------------
+  call super_test('Cylinder')
+  call assert(cyl%check(), 'Initialization')
+! Checking the normals are perpendicular one from another
+  call assert(dot_product(cyl%normal1, cyl%normal2) == 0, "normals (1)")
+  call assert(dot_product(cyl%normal1, cyl%normal3) == 0, "normals (2)")
+  call assert(dot_product(cyl%normal2, cyl%normal3) == 0, "normals (3)")
+
+  ! Check that the projection is correct
+  block
+    real(dp) :: tmp(3)
+    type(ptCylinder_t) :: tmpOut
+
+    tmp = cyl%center
+    call cyl%project(tmp, tmpOut)
+    call assert(is_close(tmpOut%r, 0._dp, 1.d-10) .and. &
+         is_close(tmpOut%z, 0._dp, 1.d-10), "Project onto center")
+
+    tmp = cyl%center + cyl%normal1
+    call cyl%project(tmp, tmpOut)
+    call assert(is_close(tmpOut%r, 0._dp, 1.d-10) &
+         .and. is_close(tmpOut%z, cyl%h, 1.d-10), "Project onto axis 1")
+
+    tmp = cyl%center + cyl%normal2
+    call cyl%project(tmp, tmpOut)
+    call assert(is_close(tmpOut%r, cyl%r, 1.d-10) &
+         .and. is_close(tmpOut%z, 0._dp, 1.d-10), "Project onto axis 2")
+
+    tmp = cyl%center + cyl%normal3
+    call cyl%project(tmp, tmpOut)
+    call assert(is_close(tmpOut%r, cyl%r, 1.d-10) &
+         .and. is_close(tmpOut%z, 0._dp, 1.d-10), "Project onto axis 3")
+
+    tmp = cyl%center + cyl%normal1 + cyl%normal2
+    call cyl%project(tmp, tmpOut)
+    call assert(is_close(tmpOut%r, cyl%r, 1.d-10) &
+         .and. is_close(tmpOut%z, cyl%h, 1.d-10), "Project onto axis 1+2")
+  end block
+
+  !----------------------------------------
+  ! Testing cube
+  !----------------------------------------
+  call super_test('Cube')
+  cube%center = (/10., 1.10, 0./)
+  cube%a = 0.5
+  cube%normal1 = (/10, 1, 0/)
+  cube%normal2 = (/1, -10, 0/)
+
+  call assert(cube%check(), 'Initialization')
+
+  call assert(dot_product(cube%normal1, cube%normal2) == 0, "normals (1)")
+  call assert(dot_product(cube%normal1, cube%normal3) == 0, "normals (2)")
+  call assert(dot_product(cube%normal2, cube%normal3) == 0, "normals (3)")
+
+  block
+    real(dp) :: tmp(3)
+    type(ptCube_t) :: tmpOut
+
+    tmp = cube%center
+    call cube%project(tmp, tmpOut)
+    call assert(is_close(tmpOut%x, 0._dp, 1.d-10) .and. &
+         is_close(tmpOut%y, 0._dp, 1.d-10) .and. &
+         is_close(tmpOut%z, 0._dp, 1.d-10), &
+         "Project onto center")
+
+    tmp = cube%center + cube%normal1 * cube%a
+    call cube%project(tmp, tmpOut)
+    call assert(is_close(tmpOut%x, cube%a, 1.d-10), &
+         "Project onto axis 1")
+
+    tmp = cube%center + cube%normal2 * cube%a * 2
+    call cube%project(tmp, tmpOut)
+    call assert(is_close(tmpOut%y, cube%a * 2, 1.d-10), &
+         "Project onto axis 2")
+
+    tmp = cube%center + cube%normal3 * cube%a * (-3)
+    call cube%project(tmp, tmpOut)
+    call assert(is_close(tmpOut%z, cube%a * (-3), 1.d-10), &
+         "Project onto axis 3")
+
+    tmp = cube%center + cube%normal1 + 2*cube%normal2 - 3*cube%normal3
+    call cube%project(tmp, tmpOut)
+    call assert(is_close(tmpOut%x, 1._dp, 1.d-10) .and. &
+         is_close(tmpOut%y, 2._dp, 1.d-10) .and. &
+         is_close(tmpOut%z, -3._dp, 1.d-10), &
+         "Project onto axis 1+2+3")
+  end block
+contains
+
+  subroutine super_test(name)
+    character(len=*), intent(in) :: name
+    write(stdout, *) 'Testing ' // name
+  end subroutine super_test
+
+  subroutine assert(bool, msg)
+    logical, intent(in) :: bool
+    character(len=*), intent(in) :: msg
+
+    if (.not. bool) then
+       write(stdout, '(4x,a)') msg // '...failed!'
+       stop
+    else
+       write(stdout, '(4x,a)') msg // '...ok!'
+    end if
+  end subroutine assert
+
+  function is_close(A, B, aerr)
+    real(dp), intent(in) :: A, B, aerr
+    logical :: is_close
+
+    if (abs(A - B) < aerr) then
+       is_close = .true.
+    else
+       is_close = .false.
+    end if
+  end function is_close
+
+end program test
+
